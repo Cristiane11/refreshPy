f97d9bcad2dacbee78656573b393209c0c42c22b
Cristiane Abreu <bjwmcd@yahoo.com>
2025-09-04 09:22:03 -0400

    update

---
+++ main_class_init.py
@@ -0,0 +1,22 @@
+class officeBuilding:
+    def __init__(self, name, address, num_floors):
+        self.name = name
+        self.address = address
+        self.num_floors = num_floors        
+    def __str__(self):
+        return f"{self.name}, located at {self.address}, has {self.num_floors} floors."
+    
+building1 = officeBuilding("Tech Hub", "456 Innovation Drive", 10)
+print(building1)
+building2 = officeBuilding("Business Center", "789 Enterprise Ave", 15)
+print(building2)
+building3 = officeBuilding("Corporate Plaza", "101 Commerce St", 20)
+print(building3)
+class House:
+    def __init__(self, address, num_bedrooms, num_bathrooms):
+        self.address = address
+        self.num_bedrooms = num_bedrooms
+        self.num_bathrooms = num_bathrooms
+        
+    def __str__(self):
+        return f"House at {self.address} with {self.num_bedrooms} bedrooms and {self.num_bathrooms} bathrooms."
